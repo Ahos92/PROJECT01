bdeec21caea562e7a8357758db3b07ab5f3455b3
TW <qswaz98@naver.com>
2020-12-03 14:58:46 +0900

    console execute

--- src/project/five/pos/payment/Payment.java
+++ src/project/five/pos/payment/Payment.java
@@ -2,4 +2,12 @@ package project.five.pos.payment;
 
 public class Payment {
 	// payment merge
+	
+	// payment merge test2 
+	
+	public static void main(String[] args) {
+		int a = 19;
+		int b = 21;
+		System.out.println(a + b);
+	}
 }
