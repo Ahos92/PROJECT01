aa597a5b816db7a6582f57bdb27aa7ae7d759ca6
Ahos <Ahos@DESKTOP-LPK3PRR>
2020-12-03 18:54:41 +0900

    제발되라

--- src/project/five/pos/sale/Sale.java
+++ src/project/five/pos/sale/Sale.java
@@ -1,9 +1,50 @@
 package project.five.pos.sale;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import project.five.pos.db.DBManager;
+
 public class Sale {
 
+	static Connection conn;
+	static PreparedStatement ps;
+	static ResultSet rs;
 	
-	// 푸시 실험 - 태욱
-	// 푸시 실험 - 지원입니다
-	// 푸시 실험 - 영호
+	public Sale() {
+		
+	}
+	
+	public void showCart() {
+		
+		try {
+			conn = DBManager.getConnection();
+
+			ps = conn.prepareStatement("select cart_no, product_name||' '||termsofcondition as menu, \r\n"
+					+ "selected_item, product_price* selected_item as price \r\n"
+					+ "from cart c inner join product p using(product_no)");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				System.out.printf("%-3d\t%-10s\t%-3d\t%-10d\n",
+							rs.getInt("cart_no"),
+							rs.getString("menu"),
+							rs.getInt("selected_item"),
+							rs.getInt("price")
+							);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Sale s = new Sale();
+		s.showCart();
+	}
+	
 }
---
+++ src/project/five/pos/sale/SaleScreen.java
@@ -0,0 +1,16 @@
+package project.five.pos.sale;
+
+import javax.swing.JFrame;
+
+public class SaleScreen extends JFrame {
+
+	// 주문내역라벨
+	//	상품조회 라벨
+	// 결제, 취소 버튼
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
