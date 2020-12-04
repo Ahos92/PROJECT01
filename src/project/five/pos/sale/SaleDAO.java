99ffb3a18ea664d906ef9ab2a4fa04d56563cb9f
Ahos <Ahos@DESKTOP-LPK3PRR>
2020-12-04 15:48:58 +0900

    12/04 장바구니저장, 테스트용상품 메서드

--- src/project/five/pos/sale/SaleDAO.java
+++ src/project/five/pos/sale/SaleDAO.java
@@ -4,47 +4,110 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import project.five.pos.db.DBManager;
 
-public class Sale {
+public class SaleDAO {
 
 	static Connection conn;
 	static PreparedStatement ps;
 	static ResultSet rs;
 	
-	public Sale() {
+	SaleDTO saleDTO;
+	
+	public SaleDAO() {
 		
 	}
 	
-	public void showCart() {
+	// 테스트 메서드
+	// 상품 클릭 > 주문 내역으로 데이터 전달 메서드 
+	public SaleDTO testOrder(String name, String opt, int count) {
+		// 상품 데이터 담을 객체
+		SaleDTO saleDTO = new SaleDTO();
+		
+		conn = DBManager.getConnection();
 		
 		try {
-			conn = DBManager.getConnection();
-
-			ps = conn.prepareStatement("select cart_no, product_name||' '||termsofcondition as menu, \r\n"
-					+ "selected_item, product_price* selected_item as price \r\n"
-					+ "from cart c inner join product p using(product_no)");
-
-			rs = ps.executeQuery();
-
-			while (rs.next()) {
-				System.out.printf("%-3d\t%-10s\t%-3d\t%-10d\n",
-							rs.getInt("cart_no"),
-							rs.getString("menu"),
-							rs.getInt("selected_item"),
-							rs.getInt("price")
-							);
+			
+			// 옵션 값이 없을때
+			if (opt == null) {
+				ps = conn.prepareStatement("select *"
+						+ " from product"
+						+ " where product_name = ?"
+						);
+				ps.setString(1, name);
+				
+			} else {
+				ps = conn.prepareStatement("select *"
+						+ " from product"
+						+ " where product_name = ?"
+						+ " and termsofcondition = ?"
+						);
+				
+				ps.setString(1, name);
+				ps.setString(2, opt);
 			}
+			rs = ps.executeQuery();				
+			
+			while (rs.next()) {
+
+				String pName = rs.getString("product_name");
+				String option = rs.getString("termsofcondition"); 
+
+				saleDTO.setProduct_no(rs.getInt("product_no"));
+				saleDTO.setProduct_name(pName);
+				saleDTO.setTermsofcondition(option);
+				saleDTO.setProduct_price(rs.getInt("product_price"));
+				saleDTO.setOrder_count(count);
+				
+				System.out.printf("%s(%s) %d개 주문\n", pName, option, count);
+			}
+			
+			DBManager.p_r_c_Close(ps, rs, conn);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
+		return saleDTO;
 	}
+
 	
-	public static void main(String[] args) {
-		Sale s = new Sale();
-		s.showCart();
-	}
+	// 장바구니 내역 cart TABLE에 저장
+	public ArrayList<SaleDTO> saveCartlist(ArrayList<SaleDTO> cartlist, int cartNumber) {
+	
+		try {
+			conn = DBManager.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			String sql = "insert into cart values(?, ?, ?, ?)";
+			ps = conn.prepareStatement(sql);
+			
+			for (int i = 0; i < cartlist.size(); i++) {
+
+				ps.setInt(1, i + 1); // cart_no
+				ps.setInt(2, cartNumber); // order_no
+				ps.setInt(3, cartlist.get(i).getProduct_no()); // product_no
+				ps.setInt(4, cartlist.get(i).getOrder_count()); // selected_count				
+				
+				ps.addBatch();
+				
+			}
+			
+			int[] rows = ps.executeBatch();
+			System.out.println(rows.length + "행이 변경 되었습니다.");
+			
+			DBManager.p_c_Close(ps, conn);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return cartlist;
+	}	
+
+	
 	
 }
